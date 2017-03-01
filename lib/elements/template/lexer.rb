52229f29d6259c9b9cc911f7486715e2c4b6c274
Chris Mather <chris@eventedmind.com>
2017-02-28 17:46:30 -0800

    refactor lexer to make easier to understand

--- lib/elements/template/lexer.rb
+++ lib/elements/template/lexer.rb
@@ -3,6 +3,15 @@ require "elements/template/location"
 
 module Elements
   module Template
+    module States
+      DEFAULT             = :default
+      TEMPLATE            = :template
+      TAG_NAME            = :tag_name
+      TAG_ATTRIBUTES      = :tag_attributes
+      TAG_ATTRIBUTE_VALUE = :tag_attribute_value
+      TAG_CLOSE           = :tag_close
+    end
+
     class Token
       attr_reader :type, :value, :location
       def initialize(type, value, location)
@@ -14,268 +23,192 @@ module Elements
       end
     end
 
-    class Lexer
-      class LexerError < StandardError; end
+    class Rule
+      attr_reader :matcher
+      attr_reader :action
+      def initialize(matcher, &action)
+        @matcher = matcher
+        @action = action
+      end
+    end
 
-      module Matchers
-        # spaces and newlines
-        SPACE                                = /\s/
-        TAB                                  = /\t/
-        WHITESPACE                           = /#{SPACE}|#{TAB}/
-        NEWLINE                              = /\r?\n/
-        SEA_WS                               = /#{WHITESPACE}|#{NEWLINE}/
-
-        # templates
-        # XXX make this work inline too
-        TEMPLATE_OPEN                        = /<template/
-        TEMPLATE_CLOSE                       = /<\/template>/
-
-        # tag symbols
-        OPEN_CARET_FORWARD_SLASH             = /<\//
-        # FIXME open caret should exclude <template so we don't end up with a
-        # nested template tag. or actually nested templates are a good thing so
-        # we'll come back to this idea.
-        OPEN_CARET                           = /</
-        CLOSE_CARET                          = />/
-        FORWARD_SLASH_CLOSE_CARET            = /\/>/
-        EQUALS                               = /=/
-
-        # tag and view names
-        TAG_NAME_PART                        = /[a-z][a-z0-9]*/
-        TAG_NAME                             = /#{TAG_NAME_PART}(?:-#{TAG_NAME_PART})*/
-        TAG_NAMESPACE                        = /([a-z]*):/
-        VIEW_NAME_PART                       = /[A-Z]\w+/
-        VIEW_NAME                            = /#{VIEW_NAME_PART}(?:::#{VIEW_NAME_PART})*/
-
-        # attributes
-        ATTRIBUTE_NAME                       = TAG_NAME
-        ATTRIBUTE_VALUE_SINGLE_QUOTED_STRING = /'([^']+)'/
-        ATTRIBUTE_VALUE_DOUBLE_QUOTED_STRING = /"([^"]+)"/
-        ATTRIBUTE_VALUE_CHAR                 = /-|_|\.|\/|\+|,|\?|=|:|;|#|[0-9a-zA-Z]/
-        ATTRIBUTE_VALUE_CHARS                = /#{ATTRIBUTE_VALUE_CHAR}+/
-        ATTRIBUTE_VALUE_HEXCHARS             = /#[0-9a-fA-F]+/
-        ATTRIBUTE_VALUE_PCTCHARS             = /[0-9]+%/
-
-        # any, comment and text
-        ANY                                  = /(?:(?!#{TEMPLATE_OPEN}).)+/m
-        COMMENT                              = /<!--\s*(.*?)\s*-->/m
-        TEXT                                 = /(?:(?!#{OPEN_CARET}).)+/m
+    class RuleSet
+      def initialize
+        @rules = {}
       end
 
-      attr_reader :lookahead
-      attr_reader :source
-      attr_accessor :state
+      def [](state)
+        @rules[state] ||= []
+      end
+    end
 
-      def initialize(io, opts = {})
+    module Matchers
+      # spaces and newlines
+      SPACE                                = /\s/
+      TAB                                  = /\t/
+      WHITESPACE                           = /#{SPACE}|#{TAB}/
+      NEWLINE                              = /\r?\n/
+      SEA_WS                               = /#{WHITESPACE}|#{NEWLINE}/
+
+      # templates
+      TEMPLATE_OPEN                        = /<template/
+      TEMPLATE_CLOSE                       = /<\/template>/
+
+      # tag symbols
+      OPEN_CARET_FORWARD_SLASH             = /(?:(?!#{TEMPLATE_CLOSE})<\/)/
+      OPEN_CARET                           = /(?:(?!#{TEMPLATE_OPEN})<)/
+      CLOSE_CARET                          = />/
+      FORWARD_SLASH_CLOSE_CARET            = /\/>/
+      EQUALS                               = /=/
+
+      # tag and view names
+      TAG_NAME_PART                        = /[a-z][a-z0-9]*/
+      TAG_NAME                             = /#{TAG_NAME_PART}(?:-#{TAG_NAME_PART})*/
+      TAG_NAMESPACE                        = /([a-z]*):/
+      ELEMENT_NAME                         = TAG_NAME
+      VIEW_NAME_PART                       = /[A-Z]\w+/
+      VIEW_NAME                            = /#{VIEW_NAME_PART}(?:::#{VIEW_NAME_PART})*/
+
+      # attributes
+      ATTRIBUTE_NAME                       = TAG_NAME
+      ATTRIBUTE_VALUE_SINGLE_QUOTED_STRING = /'([^']+)'/
+      ATTRIBUTE_VALUE_DOUBLE_QUOTED_STRING = /"([^"]+)"/
+      ATTRIBUTE_VALUE_CHAR                 = /-|_|\.|\/|\+|,|\?|=|:|;|#|[0-9a-zA-Z]/
+      ATTRIBUTE_VALUE_CHARS                = /#{ATTRIBUTE_VALUE_CHAR}+/
+      ATTRIBUTE_VALUE_HEXCHARS             = /#[0-9a-fA-F]+/
+      ATTRIBUTE_VALUE_PCTCHARS             = /[0-9]+%/
+
+      # any, comment and text
+      ANY                                  = /(?:(?!#{TEMPLATE_OPEN}).)+/m
+      COMMENT                              = /<!--\s*(.*?)\s*-->/m
+      TEXT                                 = /(?:(?!#{OPEN_CARET}).)+/m
+    end
+
+    class Lexer
+      class << self
+        def rules
+          @rules ||= RuleSet.new
+        end
+
+        def state(name, &block)
+          begin
+            saved_state = @current_state
+            @current_state = name
+            yield
+          ensure
+            @current_state = saved_state
+          end
+        end
+
+        def current_state
+          @current_state || States::DEFAULT
+        end
+
+        def match(matcher, state = current_state, &action)
+          rules[state] << Rule.new(matcher, &action)
+        end
+      end
+
+      state States::DEFAULT do
+        match(Matchers::ANY)                                  { token(:ANY, @scanner.matched) }
+        match(Matchers::TEMPLATE_OPEN)                        { push_state(States::TEMPLATE); push_state(States::TAG_ATTRIBUTES); token(:TEMPLATE_OPEN, @scanner.matched) }
+      end
+
+      state States::TEMPLATE do
+        match(Matchers::COMMENT)                              { token(:COMMENT, @scanner[1]) }
+        match(Matchers::TEMPLATE_OPEN)                        { push_state(States::TEMPLATE); token(:TEMPLATE_OPEN, @scanner.matched) }
+        match(Matchers::TEMPLATE_CLOSE)                       { pop_state; token(:TEMPLATE_CLOSE, @scanner.matched) }
+        match(Matchers::OPEN_CARET_FORWARD_SLASH)             { push_state(States::TAG_CLOSE); token(:OPEN_CARET_FORWARD_SLASH, @scanner.matched) }
+        match(Matchers::OPEN_CARET)                           { push_state(States::TAG_NAME); token(:OPEN_CARET, @scanner.matched) }
+        match(Matchers::TEXT)                                 { token(:TEXT, @scanner.matched) }
+      end
+
+      state States::TAG_NAME do
+        match(Matchers::TAG_NAMESPACE)                        { token(:TAG_NAMESPACE, @scanner[1]) }
+        match(Matchers::ELEMENT_NAME)                         { pop_state; push_state(States::TAG_ATTRIBUTES); token(:ELEMENT_NAME, @scanner.matched) }
+        match(Matchers::VIEW_NAME)                            { pop_state; push_state(States::TAG_ATTRIBUTES); token(:VIEW_NAME, @scanner.matched) }
+      end
+
+      state States::TAG_ATTRIBUTES do
+        match(Matchers::ATTRIBUTE_NAME)                       { token(:ATTRIBUTE_NAME, @scanner.matched) }
+        match(Matchers::EQUALS)                               { push_state(States::TAG_ATTRIBUTE_VALUE); token(:EQUALS, @scanner.matched) }
+        match(Matchers::FORWARD_SLASH_CLOSE_CARET)            { pop_state; token(:FORWARD_SLASH_CLOSE_CARET, @scanner.matched) }
+        match(Matchers::CLOSE_CARET)                          { pop_state; token(:CLOSE_CARET, @scanner.matched) }
+      end
+
+      state States::TAG_ATTRIBUTE_VALUE do
+        match(Matchers::ATTRIBUTE_VALUE_SINGLE_QUOTED_STRING) { pop_state; token(:ATTRIBUTE_VALUE, @scanner[1]) }
+        match(Matchers::ATTRIBUTE_VALUE_DOUBLE_QUOTED_STRING) { pop_state; token(:ATTRIBUTE_VALUE, @scanner[1]) }
+        match(Matchers::ATTRIBUTE_VALUE_HEXCHARS)             { pop_state; token(:ATTRIBUTE_VALUE, @scanner.matched) }
+        match(Matchers::ATTRIBUTE_VALUE_PCTCHARS)             { pop_state; token(:ATTRIBUTE_VALUE, @scanner.matched) }
+        match(Matchers::ATTRIBUTE_VALUE_CHARS)                { pop_state; token(:ATTRIBUTE_VALUE, @scanner.matched) }
+      end
+
+      state States::TAG_CLOSE do
+        match(Matchers::TAG_NAMESPACE)                        { token(:TAG_NAMESPACE, @scanner[1]) }
+        match(Matchers::ELEMENT_NAME)                         { token(:ELEMENT_NAME, @scanner.matched) }
+        match(Matchers::VIEW_NAME)                            { token(:VIEW_NAME, @scanner.matched) }
+        match(Matchers::CLOSE_CARET)                          { pop_state; token(:CLOSE_CARET, @scanner.matched) }
+      end
+
+      def initialize(io, **options)
+        @options = options.dup
         @source = if io.respond_to?(:read) then io.read else io; end
         @scanner = StringScanner.new(@source)
         @location_index = LocationIndex.new
         @lookahead = nil
-        @state = opts[:state] || :default
+        @states = []
+        @states << @options[:state] unless @options[:state].nil?
       end
 
-      def state?(value)
-        @state == value
+      def state
+        @states.last || States::DEFAULT
+      end
+
+      def push_state(state)
+        @states.push(state)
+      end
+
+      def pop_state
+        @states.pop
       end
 
       def scan
+        # if we're at the end of the input stream then return the end of file
+        # (EOF) token.
         if @scanner.eos?
-          @lookahead = Token.new(:EOF, nil, nil) if @scanner.eos?
-          return @lookahead
+          return Token.new(:EOF, nil, nil)
         end
 
-        case @state
-        when :default         then scan_in_default_state
-        when :template        then scan_in_template_state
-        when :open_tag        then scan_in_open_tag_state
-        when :attributes      then scan_in_attributes_state
-        when :attribute_value then scan_in_attribute_value_state
-        when :close_tag       then scan_in_close_tag_state
+        # if we encounter whitespace just return that to the parser so it can
+        # decide what to do.
+        if @scanner.scan(Matchers::SEA_WS)
+          return token(:SEA_WS, @scanner.matched)
         end
-      end
 
-      def eof?
-        @lookahead && @lookahead.type == :EOF
-      end
+        # find the first rule that matches given our state and call the action
+        # callback defined for that match.
+        for rule in rules[state] do
+          if @scanner.scan(rule.matcher)
+            return instance_eval(&rule.action)
+          end
+        end
 
-      def line
-        @location_index.line
-      end
-
-      def column
-        @location_index.column
-      end
-
-      def index
-        @location_index.index
+        # if we get down here it means there were no matchers. so what we'll do
+        # is create a generic token for whatever character we encountered. and
+        # we'll let the parser decide what kind of error to raise, if any.
+        type = value = @scanner.getch
+        token(type, value)
       end
 
       private
-      def scan_in_default_state
-        case
-        when matched = @scanner.scan(Matchers::ANY)
-          type = :ANY
-
-        when matched = @scanner.scan(Matchers::TEMPLATE_OPEN)
-          type = :TEMPLATE_OPEN
-          @state = :attributes
-
-        else
-          error
-        end
-
+      def token(type, value = nil)
         location = @location_index.advance(@scanner.matched)
-        @lookahead = Token.new(type, matched, location)
+        @lookahead = Token.new(type, value, location)
       end
 
-      def scan_in_template_state
-        skip_whitespace
-
-        case
-        when @scanner.scan(Matchers::COMMENT)
-          matched = @scanner[1]
-          type = :COMMENT
-
-        when matched = @scanner.scan(Matchers::TEMPLATE_CLOSE)
-          type = :TEMPLATE_CLOSE
-          @state = :default
-
-        when matched = @scanner.scan(Matchers::OPEN_CARET_FORWARD_SLASH)
-          type = :OPEN_CARET_FORWARD_SLASH
-          @state = :close_tag
-
-        when matched = @scanner.scan(Matchers::OPEN_CARET)
-          type = :OPEN_CARET
-          @state = :open_tag
-
-        when matched = @scanner.scan(Matchers::TEXT)
-          type = :TEXT
-
-        else
-          error
-        end
-
-        location = @location_index.advance(@scanner.matched)
-        @lookahead = Token.new(type, matched, location)
-      end
-
-      def scan_in_open_tag_state
-        # tag name, ns tag name, viewname are the only acceptable things. and
-        # then we push on attributes
-        skip_whitespace
-
-        case
-        when @scanner.scan(Matchers::TAG_NAMESPACE)
-          matched = @scanner[1]
-          type = :TAG_NAMESPACE
-
-        when matched = @scanner.scan(Matchers::TAG_NAME)
-          type = :TAG_NAME
-          @state = :attributes
-
-        when matched = @scanner.scan(Matchers::VIEW_NAME)
-          type = :VIEW_NAME
-          @state = :attributes
-
-        else
-          error
-        end
-
-        location = @location_index.advance(@scanner.matched)
-        @lookahead = Token.new(type, matched, location)
-      end
-
-      def scan_in_attributes_state
-        skip_whitespace
-
-        case
-        when matched = @scanner.scan(Matchers::ATTRIBUTE_NAME)
-          type = :ATTRIBUTE_NAME
-
-        when matched = @scanner.scan(Matchers::EQUALS)
-          type = :EQUALS
-          @state = :attribute_value
-
-        when matched = @scanner.scan(Matchers::FORWARD_SLASH_CLOSE_CARET)
-          type = :FORWARD_SLASH_CLOSE_CARET
-          @state = :template
-
-        when matched = @scanner.scan(Matchers::CLOSE_CARET)
-          type = :CLOSE_CARET
-          @state = :template
-
-        else
-          error
-        end
-
-        location = @location_index.advance(@scanner.matched)
-        @lookahead = Token.new(type, matched, location)
-      end
-
-      def scan_in_attribute_value_state
-        skip_whitespace
-
-        case
-        when @scanner.scan(Matchers::ATTRIBUTE_VALUE_SINGLE_QUOTED_STRING)
-          matched = @scanner[1]
-          type = :ATTRIBUTE_VALUE
-          @state = :attributes
-
-        when @scanner.scan(Matchers::ATTRIBUTE_VALUE_DOUBLE_QUOTED_STRING)
-          matched = @scanner[1]
-          type = :ATTRIBUTE_VALUE
-          @state = :attributes
-
-        when matched = @scanner.scan(Matchers::ATTRIBUTE_VALUE_HEXCHARS)
-          type = :ATTRIBUTE_VALUE
-          @state = :attributes
-
-        when matched = @scanner.scan(Matchers::ATTRIBUTE_VALUE_PCTCHARS)
-          type = :ATTRIBUTE_VALUE
-          @state = :attributes
-
-        when matched = @scanner.scan(Matchers::ATTRIBUTE_VALUE_CHARS)
-          type = :ATTRIBUTE_VALUE
-          @state = :attributes
-
-        else
-          error
-        end
-
-        location = @location_index.advance(@scanner.matched)
-        @lookahead = Token.new(type, matched, location)
-      end
-
-      def scan_in_close_tag_state
-        skip_whitespace
-
-        case
-        when @scanner.scan(Matchers::TAG_NAMESPACE)
-          matched = @scanner[1]
-          type = :TAG_NAMESPACE
-        when matched = @scanner.scan(Matchers::TAG_NAME)
-          type = :TAG_NAME
-        when matched = @scanner.scan(Matchers::VIEW_NAME)
-          type = :VIEW_NAME
-        when matched = @scanner.scan(Matchers::CLOSE_CARET)
-          type = :CLOSE_CARET
-          @state = :template
-        else
-          error
-        end
-
-        location = @location_index.advance(@scanner.matched)
-        @lookahead = Token.new(type, matched, location)
-      end
-
-      def skip_whitespace
-        while @scanner.scan(Matchers::SEA_WS)
-          @location_index.advance(@scanner.matched)
-        end
-      end
-
-      def error
-        raise LexerError.new("Unexpected character: '#{@scanner.peek(1)}'. Location: #{@location_index}. State: #{@state}")
+      def rules
+        self.class.rules
       end
     end
   end
---
+++ test/elements/template/lexer_test.rb
@@ -0,0 +1,279 @@
+require "test_helper"
+require "elements/template/lexer"
+
+describe "Elements::Template::Lexer" do
+  def assert_location_index(location_index, index:, line:, column:)
+    assert_equal index, location_index.index, "@location_index.index is wrong"
+    assert_equal line, location_index.line, "@location_index.line is wrong"
+    assert_equal column, location_index.column, "@location_index.column is wrong"
+  end
+
+  def assert_token(input:, type:, value: input, start_state: :default, finish_state: start_state)
+    lexer = Elements::Template::Lexer.new(input, state: start_state == :default ? nil : start_state)
+    token = lexer.scan
+    assert token, "no token"
+    assert_equal type, token.type, "wrong token.type"
+    assert_equal value, token.value, "wrong token.value"
+    assert_equal finish_state, lexer.state, "wrong lexer finishing state"
+  end
+
+  describe "token" do
+    it "creates token with correct location" do
+      input = "one\ntwo"
+      lexer = Elements::Template::Lexer.new(input)
+
+      # test the initial location index
+      location_index = lexer.instance_eval { @location_index }
+      assert_location_index location_index, index: 0, line: 1, column: 0
+
+      # make sure we get a token back
+      token = lexer.instance_eval { @scanner.scan(/#{input}/); token(:ANY, input) }
+      assert_instance_of Elements::Template::Token, token, "no token returned from token method"
+      assert_equal :ANY, token.type, "wrong token type"
+      assert_equal input, token.value, "wrong token value"
+
+      # see if location was advanced
+      location_index = lexer.instance_eval { @location_index }
+      assert_location_index location_index, index: input.size, line: 2, column: 3
+    end
+  end
+
+  describe "States::DEFAULT" do
+    it "ANY" do
+      assert_token(
+        input: "test",
+        type: :ANY
+      )
+    end
+
+    it "TEMPLATE_OPEN" do
+      assert_token(
+        input: "<template",
+        type: :TEMPLATE_OPEN,
+        start_state: :default,
+        finish_state: :tag_attributes
+      )
+    end
+  end
+
+  describe "States::TEMPLATE" do
+    it "COMMENT" do
+      assert_token(
+        input: "<!-- my comment -->",
+        value: "my comment",
+        type: :COMMENT,
+        start_state: :template
+      )
+    end
+
+    it "TEMPLATE_OPEN" do
+      assert_token(
+        input: "<template",
+        type: :TEMPLATE_OPEN,
+        start_state: :template,
+        finish_state: :template
+      )
+    end
+
+    it "TEMPLATE_CLOSE" do
+      assert_token(
+        input: "</template>",
+        type: :TEMPLATE_CLOSE,
+        start_state: :template,
+        finish_state: :default
+      )
+    end
+
+    it "OPEN_CARET_FORWARD_SLASH" do
+      assert_token(
+        input: "</",
+        type: :OPEN_CARET_FORWARD_SLASH,
+        start_state: :template,
+        finish_state: :tag_close
+      )
+    end
+
+    it "OPEN_CARET" do
+      assert_token(
+        input: "<",
+        type: :OPEN_CARET,
+        start_state: :template,
+        finish_state: :tag_name
+      )
+    end
+
+    it "TEXT" do
+      assert_token(
+        input: "one\ntwo\nthree\tfour five",
+        type: :TEXT,
+        start_state: :template,
+        finish_state: :template
+      )
+    end
+  end
+
+  describe "States::TAG_NAME" do
+    it "TAG_NAMESPACE" do
+      assert_token(
+        input: "ns:tag",
+        type: :TAG_NAMESPACE,
+        value: "ns",
+        start_state: :tag_name,
+        finish_state: :tag_name
+      )
+    end
+
+    it "ELEMENT_NAME" do
+      assert_token(
+        input: "div",
+        type: :ELEMENT_NAME,
+        start_state: :tag_name,
+        finish_state: :tag_attributes
+      )
+    end
+
+    it "VIEW_NAME" do
+      assert_token(
+        input: "Views::Home::Template",
+        type: :VIEW_NAME,
+        start_state: :tag_name,
+        finish_state: :tag_attributes
+      )
+    end
+  end
+
+  describe "States::TAG_ATTRIBUTES" do
+    it "ATTRIBUTE_NAME" do
+      assert_token(
+        input: "data-attribute",
+        type: :ATTRIBUTE_NAME,
+        start_state: :tag_attributes,
+        finish_state: :tag_attributes
+      )
+
+      assert_token(
+        input: "regular",
+        type: :ATTRIBUTE_NAME,
+        start_state: :tag_attributes,
+        finish_state: :tag_attributes
+      )
+    end
+
+    it "EQUALS" do
+      # XXX this is an example where handling whitespace would be easier in the
+      # lexer! but maybe it's not too bad to do it in the parser. will see.
+      assert_token(
+        input: "=",
+        type: :EQUALS,
+        start_state: :tag_attributes,
+        finish_state: :tag_attribute_value
+      )
+    end
+
+    it "FORWARD_SLASH_CLOSE_CARET" do
+      assert_token(
+        input: "/>",
+        type: :FORWARD_SLASH_CLOSE_CARET,
+        start_state: :tag_attributes,
+        finish_state: :default
+      )
+    end
+
+    it "CLOSE_CARET" do
+      assert_token(
+        input: ">",
+        type: :CLOSE_CARET,
+        start_state: :tag_attributes,
+        finish_state: :default
+      )
+    end
+  end
+
+  describe "States::TAG_ATTRIBUTE_VALUE" do
+    it "ATTRIBUTE_VALUE_SINGLE_QUOTED_STRING" do
+      assert_token(
+        input: "'value'",
+        value: "value",
+        type: :ATTRIBUTE_VALUE,
+        start_state: :tag_attribute_value,
+        finish_state: :default
+      )
+    end
+
+    it "ATTRIBUTE_VALUE_DOUBLE_QUOTED_STRING" do
+      assert_token(
+        input: '"value"',
+        value: "value",
+        type: :ATTRIBUTE_VALUE,
+        start_state: :tag_attribute_value,
+        finish_state: :default
+      )
+    end
+
+    it "ATTRIBUTE_VALUE_HEXCHARS" do
+      assert_token(
+        input: "#333cc",
+        type: :ATTRIBUTE_VALUE,
+        start_state: :tag_attribute_value,
+        finish_state: :default
+      )
+    end
+
+    it "ATTRIBUTE_VALUE_PCTCHARS" do
+      assert_token(
+        input: "100%",
+        type: :ATTRIBUTE_VALUE,
+        start_state: :tag_attribute_value,
+        finish_state: :default
+      )
+    end
+
+    it "ATTRIBUTE_VALUE_CHARS" do
+      assert_token(
+        input: "one-two-123_/+,?=:;#ABC",
+        type: :ATTRIBUTE_VALUE,
+        start_state: :tag_attribute_value,
+        finish_state: :default
+      )
+    end
+  end
+
+  describe "States::TAG_CLOSE" do
+    it "TAG_NAMESPACE" do
+      assert_token(
+        input: "ns:div",
+        value: "ns",
+        type: :TAG_NAMESPACE,
+        start_state: :tag_close,
+        finish_state: :tag_close
+      )
+    end
+
+    it "ELEMENT_NAME" do
+      assert_token(
+        input: "div",
+        type: :ELEMENT_NAME,
+        start_state: :tag_close,
+        finish_state: :tag_close
+      )
+    end
+
+    it "VIEW_NAME" do
+      assert_token(
+        input: "Views::Home",
+        type: :VIEW_NAME,
+        start_state: :tag_close,
+        finish_state: :tag_close
+      )
+    end
+
+    it "CLOSE_CARET" do
+      assert_token(
+        input: ">",
+        type: :CLOSE_CARET,
+        start_state: :tag_close,
+        finish_state: :default
+      )
+    end
+  end
+end
