95d94348f58776ba692254de22e46574363fd79b
Chris Mather <chris@eventedmind.com>
2017-03-03 13:40:10 -0800

    added new assertion assert_in_types

--- lib/elements/assertions.rb
+++ lib/elements/assertions.rb
@@ -6,9 +6,19 @@ module Elements
       end
     end
 
+    def assert_in_types(types, value)
+      raise TypeError.new("Got value of type '#{value.class}' but expected one of: [#{types.join(', ')}]") unless types.include?(value.class)
+    end
+
     def assert_type_or_nil(type, *args)
       args.each do |object|
         raise TypeError, "Expected #{object} to be of type #{type}." unless object.is_a?(type) || object.nil?
+      end
+    end
+
+    def assert_respond_to(method, *args)
+      args.each do |object|
+        raise TypeError, "Expected #{object} to respond to #{method}." unless object.respond_to?(method)
       end
     end
   end
